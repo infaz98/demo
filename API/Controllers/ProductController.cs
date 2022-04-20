44b139a646362039ff07a26ea4f798726f423682
infaz <infaz.rumy@digiratina.com>
2022-04-20 16:49:20 +0530

    refactor

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new JsonResult("Test");
+        }
+    }
+}
